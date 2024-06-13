7a46ce1e487fe6a67153bb14f746e4ff79ddff5e
MustafaYacoub85 <mustafa.yacoub2013@gmail.com>
2024-06-13 19:32:59 +0300

    Commit 3 Исправил первую часть JDBC

--- Task-1-master/src/main/java/jm/task/core/jdbc/Main.java
+++ Task-1-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import org.postgresql.Driver;
 
@@ -12,19 +13,24 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Class<Driver> driverClass = Driver.class;
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
 
-        userDao.createUsersTable();
+        UserServiceImpl userDao = new UserServiceImpl();
+
+//        userDao.createUsersTable();
+
         userDao.dropUsersTable();
-        userDao.saveUser("Ivan","Ivanov", (byte) 38);
-        userDao.saveUser("Petr","Petrov", (byte) 42);
-        userDao.saveUser("Aleksandar","Aleksandrovech", (byte) 21);
-        userDao.saveUser("Maksim","Maksimovich", (byte) 31);
-        userDao.removeUserById(4);
-        List<User> userList1 = userDao.getAllUsers();
-        userList1.forEach(System.out::println);
-        userDao.cleanUsersTable();
+
+//        userDao.saveUser("Ivan","Ivanov", (byte) 38);
+//        userDao.saveUser("Petr","Petrov", (byte) 42);
+//        userDao.saveUser("Aleksandar","Aleksandrovech", (byte) 21);
+//        userDao.saveUser("Maksim","Maksimovich", (byte) 31);
+////
+//        userDao.removeUserById(4);
+//
+//        List<User> userList1 = userDao.getAllUsers();
+//        userList1.forEach(System.out::println);
+//
+//        userDao.cleanUsersTable();
             // реализуйте алгоритм здесь
     }
 }
